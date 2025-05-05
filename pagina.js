cafd477ce4c6343313e21fb712701d7c8d81df13
thaylan <thaylan92@gmail.com>
2025-05-05 19:28:01 -0300

    Add files via upload

---
+++ pagina.js
@@ -0,0 +1,4 @@
+document.getElementById("botao").addEventListener("click", function(){
+    document.getElementById("frase").innerHTML = "Porque sou eu que conheço os planos que tenho para vocês, planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro. Jeremias 29:11. JESUS TE AMA!";
+    
+});
